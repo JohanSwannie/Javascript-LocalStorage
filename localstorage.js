8de02402a944888fb0f3962b29674886748d7c77
jswan <jswannieman@gmail.com>
2023-04-03 15:08:01 +1000

    Commit localstorage Javascript Component

---
+++ localstorage.js
@@ -0,0 +1,27 @@
+const counter = document.getElementById("count");
+
+function addToCounter() {
+  var count = Number(window.localStorage.getItem("count"));
+  count += 1;
+  localStorage.setItem("count", count);
+  counter.innerHTML = count;
+}
+
+function subtractFromCounter() {
+  var count = Number(window.localStorage.getItem("count"));
+  count -= 1;
+  localStorage.setItem("count", count);
+  counter.innerHTML = count;
+}
+
+function multiplyCounter() {
+  var count = Number(window.localStorage.getItem("count"));
+  count *= 10;
+  localStorage.setItem("count", count);
+  counter.innerHTML = count;
+}
+
+function clearCounter() {
+  localStorage.clear();
+  counter.innerHTML = 0;
+}
