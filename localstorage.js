ce0297531a6cb890dee6ce13a2c680161054f85d
jswan <jswannieman@gmail.com>
2023-04-03 15:09:54 +1000

    Commit localstorage Javascript Component

--- localstorage.js
+++ localstorage.js
@@ -1,21 +1,21 @@
 const counter = document.getElementById("count");
 
 function addToCounter() {
-  var count = Number(window.localStorage.getItem("count"));
+  var count = Number(localStorage.getItem("count"));
   count += 1;
   localStorage.setItem("count", count);
   counter.innerHTML = count;
 }
 
 function subtractFromCounter() {
-  var count = Number(window.localStorage.getItem("count"));
+  var count = Number(localStorage.getItem("count"));
   count -= 1;
   localStorage.setItem("count", count);
   counter.innerHTML = count;
 }
 
 function multiplyCounter() {
-  var count = Number(window.localStorage.getItem("count"));
+  var count = Number(localStorage.getItem("count"));
   count *= 10;
   localStorage.setItem("count", count);
   counter.innerHTML = count;
